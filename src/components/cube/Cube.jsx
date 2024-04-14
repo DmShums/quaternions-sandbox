0f1bfb8c25422fc5878e572491b43ca551626dd4
ParubokMotorniy <oles.yarish@gmail.com>
2024-04-15 02:32:54 +0300

    Swiping works so far..

--- src/components/cube/Cube.jsx
+++ src/components/cube/Cube.jsx
@@ -3,10 +3,12 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import * as dat from "dat.gui"; // Import dat.gui
 import { InteractionManager } from 'three.interactive';
+import { cube } from "mathjs";
 
 const Cube = () => {
   const containerRef = useRef(null);
   const cubeRef = useRef(null); // Reference to the cube mesh
+  const rotationStruct = useRef(null);
 
   useEffect(() => {
     const w = window.innerWidth;
@@ -57,19 +59,161 @@ const Cube = () => {
     cubeRotationFolder.add(rotation, "y", 0, 360).name("Rotation Y");
     cubeRotationFolder.add(rotation, "z", 0, 360).name("Rotation Z");
 
+    //Swipe rotation code
+    const cubeRotationStruct={
+      mouseDown : false,
+      rotateStartPoint : new THREE.Vector3(0, 0, 1),
+      rotateEndPoint : new THREE.Vector3(0, 0, 1),
+      curQuaternion : new THREE.Quaternion(),
+      windowHalfX : window.innerWidth / 2,
+      windowHalfY : window.innerHeight / 2,
+      rotationSpeed : 1500,
+      lastMoveTimestamp : new Date(),
+      moveReleaseTimeDelta : 50,
+      startPoint : {
+        x: 0,
+        y: 0
+      },
+      deltaX : 0,
+      deltaY : 0
+    }
+
+    rotationStruct.current = cubeRotationStruct;
+
+    function onWindowResize()
+    {
+      windowHalfX = window.innerWidth / 2;
+      windowHalfY = window.innerHeight / 2;
+  
+      camera.aspect = window.innerWidth / window.innerHeight;
+      camera.updateProjectionMatrix();
+  
+      renderer.setSize(window.innerWidth, window.innerHeight);
+    }
+  
+    function onDocumentMouseDown(event)
+    {
+      //event.preventDefault();  
+      cubeRef.current.addEventListener('mousemove', onDocumentMouseMove);
+      cubeRef.current.addEventListener('mouseup', onDocumentMouseUp);
+  
+      rotationStruct.current.mouseDown = true;
+  
+      rotationStruct.current.startPoint = {
+        x: event.coords.x,
+        y: event.coords.y
+      };
+  
+      rotationStruct.current.rotateStartPoint = rotationStruct.current.rotateEndPoint = projectOnTrackball(0, 0);
+    }
+  
+    function onDocumentMouseMove(event)
+    {
+      rotationStruct.current.deltaX = event.coords.x - rotationStruct.current.startPoint.x;
+      rotationStruct.current.deltaY = event.coords.y - rotationStruct.current.startPoint.y;
+  
+      handleRotation();
+  
+      rotationStruct.current.startPoint.x = event.coords.x;
+      rotationStruct.current.startPoint.y = event.coords.y;
+  
+      rotationStruct.current.lastMoveTimestamp = new Date();
+    }
+  
+    function onDocumentMouseUp(event)
+    {
+      if (new Date().getTime() - rotationStruct.current.lastMoveTimestamp.getTime() > rotationStruct.current.moveReleaseTimeDelta)
+      {
+        rotationStruct.current.deltaX = event.coords.x - rotationStruct.current.startPoint.x;
+        rotationStruct.current.deltaY = event.coords.y - rotationStruct.current.startPoint.y;
+      }
+  
+      rotationStruct.current.mouseDown = false;
+  
+      cubeRef.current.removeEventListener('mousemove', onDocumentMouseMove);
+      cubeRef.current.removeEventListener('mouseup', onDocumentMouseUp);
+    }
+  
+    function projectOnTrackball(touchX, touchY)
+    {
+      var mouseOnBall = new THREE.Vector3();
+  
+      mouseOnBall.set(
+        clamp(touchX / rotationStruct.current.windowHalfX, -1, 1), clamp(touchY / rotationStruct.current.windowHalfY, -1, 1),
+        0.0
+      );
+  
+      var length = mouseOnBall.length();
+  
+      if (length > 1.0)
+      {
+        mouseOnBall.normalize();
+      }
+      else
+      {
+        mouseOnBall.z = Math.sqrt(1.0 - length * length);
+      }
+  
+      return mouseOnBall;
+    }
+  
+    function rotateMatrix(rotateStart, rotateEnd)
+    {
+      var axis = new THREE.Vector3(), quaternion = new THREE.Quaternion();
+  
+      var angle = Math.acos(rotateStart.dot(rotateEnd) / rotateStart.length() / rotateEnd.length());
+  
+      if (angle)
+      {
+        axis.crossVectors(rotateStart, rotateEnd).normalize();
+        angle *= rotationStruct.current.rotationSpeed;
+        quaternion.setFromAxisAngle(axis, angle);
+      }
+      return quaternion;
+    }
+  
+    function clamp(value, min, max)
+    {
+      return Math.min(Math.max(value, min), max);
+    }
+
+    var handleRotation = function()
+    {
+      rotationStruct.current.rotateEndPoint = projectOnTrackball(rotationStruct.current.deltaX, rotationStruct.current.deltaY);
+
+      var rotateQuaternion = rotateMatrix(rotationStruct.current.rotateStartPoint, rotationStruct.current.rotateEndPoint);
+      rotationStruct.current.curQuaternion = cubeRef.current.quaternion;
+      rotationStruct.current.curQuaternion.multiplyQuaternions(rotateQuaternion, rotationStruct.current.curQuaternion);
+      rotationStruct.current.curQuaternion.normalize();
+      // cubeRef.current.setRotationFromQuaternion(rotationStruct.current.curQuaternion);
+      cubeRef.current.quaternion.copy(rotationStruct.current.curQuaternion);
+
+      rotationStruct.current.rotateEndPoint = rotationStruct.current.rotateStartPoint;
+    };
+
+    const interactionManager = new InteractionManager(
+      renderer,
+      camera,
+      renderer.domElement
+    );
+
+    interactionManager.add(cubeRef.current);
+    cubeRef.current.addEventListener('mousedown', onDocumentMouseDown);
+
     const animate = () => {
       requestAnimationFrame(animate);
+      interactionManager.update();
 
       // Update cube rotation
-      const quaternion = new THREE.Quaternion().setFromEuler(
-        new THREE.Euler(
-          THREE.MathUtils.degToRad(rotation.x),
-          THREE.MathUtils.degToRad(rotation.y),
-          THREE.MathUtils.degToRad(rotation.z),
-          "XYZ"
-        )
-      );
-      cubeRef.current.quaternion.copy(quaternion);
+      // const quaternion = new THREE.Quaternion().setFromEuler(
+      //   new THREE.Euler(
+      //     THREE.MathUtils.degToRad(rotation.x),
+      //     THREE.MathUtils.degToRad(rotation.y),
+      //     THREE.MathUtils.degToRad(rotation.z),
+      //     "XYZ"
+      //   )
+      // );
+      // cubeRef.current.quaternion.copy(quaternion);
 
       renderer.render(scene, camera);
     };
